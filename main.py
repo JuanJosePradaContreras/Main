02e9c15e6e45d978711859e8e6fd51f9de4c6cab
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-10-29 12:06:07 -0500

    Ejemplo 2

---
+++ main.py
@@ -0,0 +1,15 @@
+name = "Juan Jose Prada"   #Tipo de dato string
+gender = "M"   #Tipo de dato char
+age = 24    #Tipo de dato int o entero
+Height = 1.85     #Tipo de doble o real
+trainer = False   #Tipo de dato boolean
+
+
+#Conjuntos
+
+hobbies = ["Cocinar", "Wow Horda Tauren", "Leer"]    #Tipo de dato array
+direction = (dict) ["country" : "Colombia", "state" : "Santander"]   #Tipo de dato objeto
+
+#Imprimir
+
+print(f"""Teacher's namer: " {name} """)
---
+++ main_2.py
@@ -0,0 +1,4 @@
+fullname = input("Enter your fullname: ")
+age = int(input("Enter your age: "))
+gender = input(Enter your possiblen gender options M, F, X: ")
+height = float(input("Enter your height"))
